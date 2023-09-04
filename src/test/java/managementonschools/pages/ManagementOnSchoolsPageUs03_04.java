8ad3528ffac65bda4a21451e245c80fb519f04b3
Ahmet Gül <nnexium@gmail.com>
2023-09-04 20:55:10 +0300

    AhmetV2

--- src/test/java/managementonschools/pages/ManagementOnSchoolsPageUs03_04.java
+++ src/test/java/managementonschools/pages/ManagementOnSchoolsPageUs03_04.java
@@ -6,12 +6,55 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ManagementOnSchoolsPageUs03_04 {
-    public ManagementOnSchoolsPageUs03_04(){
-        PageFactory.initElements(Driver.getDriver(),this);
+
+
+    public ManagementOnSchoolsPageUs03_04() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
-    @FindBy(xpath = "//*[@class='header_link me-2']")
-    public WebElement register;
-    @FindBy(xpath = "//*[@id='name']")
-    public WebElement studentName;
+
+    @FindBy(xpath = "//*[@class='header_link ms-2']")
+    public WebElement login;
+
+    @FindBy(xpath = "//*[@id='username']")
+    public WebElement username;
+
+    @FindBy(xpath = "//*[@id=\"offcanvasNavbar-expand-lg\"]/div[2]/div/a[5]")
+    public WebElement contactbutton;
+
+    @FindBy(xpath = "//*[@class='fw-semibold btn btn-primary']")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//*[@placeholder='Name']")
+    public WebElement yourName;
+
+
+    @FindBy(xpath = "//*[@placeholder='Email']")
+    public WebElement yourEmail;
+
+    @FindBy(xpath = "//*[@placeholder='Subject']")
+    public WebElement Subject;
+
+    @FindBy(xpath = "//*[@placeholder='Message']")
+    public WebElement Message;
+
+    @FindBy(xpath = "//*[@class='fw-semibold btn btn-primary']")
+    public WebElement sendMessage;
+
+    @FindBy(css = ".Toastify__toast-body")
+    public WebElement assertName;
+
+    @FindBy(xpath = "//*[@class='Toastify__toast-container Toastify__toast-container--top-center']")
+    public WebElement assertEmail;
+
+    @FindBy(xpath = "//*[@id=\"27\"]")
+    public WebElement assertSubject;
+
+    @FindBy(css = ".Toastify__toast-body")//class
+    public WebElement assertMessage;
+
+
+
+
 
 }
+
--- src/test/java/managementonschools/stepDefinition/ManagementOnSchoolsStepDefinitionUs01_02.java
+++ src/test/java/managementonschools/stepDefinition/ManagementOnSchoolsStepDefinitionUs01_02.java
@@ -1,45 +1,9 @@
 package managementonschools.stepDefinition;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.But;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import managementonschools.pages.ManagementOnSchoolsPageUs01_02;
-import managementonschools.utilities.ConfigReader;
-import managementonschools.utilities.Driver;
 
 public class ManagementOnSchoolsStepDefinitionUs01_02 {
 
     ManagementOnSchoolsPageUs01_02 managementOnSchoolsPage = new ManagementOnSchoolsPageUs01_02();
-    @Given("kullanici ana sayfaya gider")
-    public void kullanicianasayfayagider() {
-        Driver.getDriver().get(ConfigReader.getProperty("managementonschoolsUrl"));
-    }
 
-    @Then("kullanici register butonuna tiklar")
-    public void kullaniciregisterbutonunatiklar() {
-        managementOnSchoolsPage.register.click();
-    }
-
-
-    @And("kullanici name girer")
-    public void kullanicinamegirer() {
-        managementOnSchoolsPage.studentName.sendKeys("fatih");
-    }
-
-    
-
-    @But("kullanici {int} saniye bekler")
-    public void kullaniciSaniyeBekler(int sayi) {
-        try {
-            Thread.sleep(sayi*1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @And("sayfayi kapatir")
-    public void sayfayiKapatir() {
-       Driver.closeDriver();
-    }
 }
