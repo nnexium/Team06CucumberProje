5e7f8957636eba0a17b691bc91bb78aacdcabc36
Ahmet Gül <nnexium@gmail.com>
2023-09-08 01:03:05 +0300

    AhmetV6

--- src/test/java/managementonschools/stepDefinition/ManagementOnSchoolsStepDefinitionUs03_04.java
+++ src/test/java/managementonschools/stepDefinition/ManagementOnSchoolsStepDefinitionUs03_04.java
@@ -1,6 +1,7 @@
 package managementonschools.stepDefinition;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.But;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import managementonschools.pages.ManagementOnSchoolsPageUs03_04;
@@ -44,6 +45,7 @@ public class ManagementOnSchoolsStepDefinitionUs03_04 {
     @Then("Login'e tıkla")
     public void loginETıkla() {
         managementOnSchools.loginButton.click();
+        ReusableMethods.bekle(3);
     }
 
     @Given(": Kullanıcı Your Name {string} kısmına bilgileri yazar")
@@ -85,7 +87,7 @@ public class ManagementOnSchoolsStepDefinitionUs03_04 {
 
     @And(": Sayfayı kapatır")
     public void sayfayıKapatır() {
-        Driver.closeDriver();
+       // Driver.closeDriver();
     }
 
     @Then(": {string} yazısının Your Email kısmı için görüldüğünü doğrula.")
@@ -108,12 +110,15 @@ public class ManagementOnSchoolsStepDefinitionUs03_04 {
     }
     @Then("Sol tarafta açılan {string} kısmına tıklar")
     public void solTaraftaAcılanKısmınaTıklar(String arg0) {
-        managementOnSchools.menuButton.click();
-        managementOnSchools.adminManagement.click();
+        ReusableMethods.scroll(managementOnSchools.deanEnter);
+        ReusableMethods.bekle(2);
+        ReusableMethods.click(managementOnSchools.deanEnter);
+        ReusableMethods.bekle(2);
     }
 
     @Given(": Kullanıcı Name {string} kısmına bilgileri yazar")
     public void kullanıcıNameKısmınaBilgileriYazar(String arg0) {
+        managementOnSchools.nameEnter.sendKeys("Dean",Keys.TAB,"Team06",Keys.TAB,"San Diego");
     }
 
     @Then(": Kullanıcı Surname {string} kısmına bilgileri yazar")
@@ -122,59 +127,87 @@ public class ManagementOnSchoolsStepDefinitionUs03_04 {
 
     @Then(": Kullanıcı BirthPlace kısmına bilgileri yazar")
     public void kullanıcıBirthPlaceKısmınaBilgileriYazar() {
+
+
     }
 
     @Then(": Gender kısmında cinsiyet seçili olmadığını gör")
     public void genderKısmındaCinsiyetSeciliOlmadıgınıGor() {
+        managementOnSchools.femaleGender.isSelected();
+        ReusableMethods.bekle(1);
+        managementOnSchools.maleGender.isSelected();
+        ReusableMethods.bekle(1);
     }
 
     @Then(": Gender Kısmında cinsiyet seçimini yap")
     public void genderKısmındaCinsiyetSeciminiYap() {
+        managementOnSchools.maleGender.click();
+        ReusableMethods.bekle(1);
     }
 
     @Then(": Kullanıcı Doğum Tarihi seçer")
     public void kullanıcıDogumTarihiSecer() {
+        managementOnSchools.birthDayselect.sendKeys("22022008");
+        ReusableMethods.bekle(1);
     }
 
     @Then(": Kullanıcı Phone {string} bilglerini yazar")
     public void kullanıcıPhoneBilgleriniYazar(String arg0) {
+        managementOnSchools.phoneNumber.sendKeys("538-528-3344");
+        ReusableMethods.bekle(2);
     }
 
     @Then(": Kullanıcı SSN no {string} bilgilerini yazar")
     public void kullanıcıSSNNoBilgileriniYazar(String arg0) {
+        managementOnSchools.ssnNumber.sendKeys("123-55-5666");
+        ReusableMethods.bekle(1);
     }
 
     @Then(": Kullanıcı User Name {string} bilgilerini yazar")
     public void kullanıcıUserNameBilgileriniYazar(String arg0) {
+        managementOnSchools.UserName.sendKeys("Team");
     }
 
     @Then(": Kullanıcı Password {string} bilgilerini yazar")
     public void kullanıcıPasswordBilgileriniYazar(String arg0) {
+        managementOnSchools.password.sendKeys("Team0612");
     }
 
     @Then(": Kullanıcı Submit Buttonuna tıklar")
     public void kullanıcıSubmitButtonunaTıklar() {
+        managementOnSchools.submitButton.click();
+        ReusableMethods.bekle(2);
     }
 
     @Then(": Kullanıcı Başarı ile Dean kayıt edildiği mesajınının görüldüğünü doğrula")
     public void kullanıcıBasarıIleDeanKayıtEdildigiMesajınınınGoruldugunuDogrula() {
+        Assert.assertTrue(managementOnSchools.assertEmail.isDisplayed());
     }
 
     @Then(": Kullanıcı Phone {string} bilglerini rakamlar arasında {string} işareti olmadan yazar")
     public void kullanıcıPhoneBilgleriniRakamlarArasındaIsaretiOlmadanYazar(String arg0) {
+        managementOnSchools.phoneNumber.sendKeys("538-528-3344");
+        ReusableMethods.bekle(2);
     }
 
     @Then(": Kullanıcı SSN no {string} bilglerini rakamlar arasında {string} işareti olmadan yazar")
     public void kullanıcıSSNNoBilgleriniRakamlarArasındaIsaretiOlmadanYazar(String arg0) {
+        managementOnSchools.ssnNumber.sendKeys("123-55--566");
     }
 
     @Then(": Kullanıcı çıkan hata mesajının göründüğünü doğrula")
     public void kullanıcıCıkanHataMesajınınGorundugunuDogrula() {
+        Assert.assertTrue(managementOnSchools.assertEmail.isDisplayed());
+
+    }
+    @Then("kullanıcı bilgileri gir")
+    public void kullanıcıBilgileriGir() {
+        managementOnSchools.enter.sendKeys("Team06Admin",Keys.TAB,"Team06Admin");
     }
 
-    @Then("us gir pas gir")
-    public void usGirPasGir() {
-        managementOnSchools.username.sendKeys("AdminBatch151 ", Keys.TAB, "Batch151+");
-        ReusableMethods.bekle(2);
+    @But("Menu Butonuna Tıkla")
+    public void menuButonunaTıkla() {
+        ReusableMethods.click(managementOnSchools.menuButton);
+        ReusableMethods.bekle(4);
     }
 }
--- src/test/resources/features/US04.feature
+++ src/test/resources/features/US04.feature
@@ -1,19 +1,20 @@
-@US04
+
 Feature: US04 Managementshcool Sayfasi Testi
 
   Scenario: TC01 Kullanıcı login olma
     Given : kullanici managementonschool sayfasina gider
     Then Login butonuna tıklar
-    Then us gir pas gir
+    Then kullanıcı bilgileri gir
     Then Login'e tıkla
+    Then Menu Butonuna Tıkla
     Then Sol tarafta açılan "Dean Management" kısmına tıklar
 
-  Scenario Outline: TC01 Kullanıcı Contact Kısmından Mesaj Gönderebilme
+  Scenario Outline: TC01 Kullanıcı Dean Ekleyebilmeli
     Given : Kullanıcı Name "<Name>" kısmına bilgileri yazar
     Then : Kullanıcı Surname "<Surname>" kısmına bilgileri yazar
-    Then : Kullanıcı BirthPlace kısmına bilgileri yazar
     Then : Gender kısmında cinsiyet seçili olmadığını gör
     Then : Gender Kısmında cinsiyet seçimini yap
+    Then : Kullanıcı BirthPlace kısmına bilgileri yazar
     Then : Kullanıcı Doğum Tarihi seçer
     Then : Kullanıcı Phone "<Phone>" bilglerini yazar
     Then : Kullanıcı SSN no "<SSN no>" bilgilerini yazar
@@ -22,24 +23,23 @@ Feature: US04 Managementshcool Sayfasi Testi
     Then : Kullanıcı Submit Buttonuna tıklar
     Then : Kullanıcı Başarı ile Dean kayıt edildiği mesajınının görüldüğünü doğrula
     And  : Sayfayı kapatır
-    Examples:
-      | Name | Surname | User Name  | Phone        | SSN no      | Password |
-      | Dean | Team    | DeanTeam06 | 538-528-3344 | 123-555-666 | Team0612 |
 
+    Examples:
+      | Name | Surname | User Name  | Phone        | SSN no     | Password |
+      | Dean | Team    | DeanTeam06 | 538-528-3344 | 123-555666 | Team0612 |
+  @US04
   Scenario: TC02 Kullanıcı login olma
     Given : kullanici managementonschool sayfasina gider
     Then Login butonuna tıklar
-    Then Admin User Name  ve Admin password bilgilerini gir
+    Then kullanıcı bilgileri gir
     Then Login'e tıkla
-    Then Login butonuna tıklar
+    Then Menu Butonuna Tıkla
     Then Sol tarafta açılan "Dean Management" kısmına tıklar
-
-  Scenario Outline: TC01 Kullanıcı Contact Kısmından Mesaj Gönderebilme
+  @US04
+  Scenario Outline: TC02 Kullanıcı Dean Ekleme hatasını görebilmeli
     Given : Kullanıcı Name "<Name>" kısmına bilgileri yazar
     Then : Kullanıcı Surname "<Surname>" kısmına bilgileri yazar
     Then : Kullanıcı BirthPlace kısmına bilgileri yazar
-    Then : Gender kısmında cinsiyet seçili olmadığını gör
-    Then : Gender Kısmında cinsiyet seçimini yap
     Then : Kullanıcı Doğum Tarihi seçer
     Then : Kullanıcı Phone "<Phone>" bilglerini rakamlar arasında "-" işareti olmadan yazar
     Then : Kullanıcı SSN no "<SSN no>" bilglerini rakamlar arasında "-" işareti olmadan yazar
@@ -47,8 +47,9 @@ Feature: US04 Managementshcool Sayfasi Testi
     Then : Kullanıcı Password "<Password>" bilgilerini yazar
     Then : Kullanıcı Submit Buttonuna tıklar
     Then : Kullanıcı çıkan hata mesajının göründüğünü doğrula
-    And  : Sayfayı kapatırg
+    And  : Sayfayı kapatır
+
     Examples:
-      | Name | Surname | User Name  | Phone      | SSN no    | Password |
-      | Dean | Team    | DeanTeam06 | 5385283344 | 123555666 | Team0612 |
+      | Name | Surname | User Name  | Phone        | SSN no      | Password |
+      | Dean | Team    | DeanTeam06 | 538-528-3344 | 123-55-5666 | Team0612 |
 
